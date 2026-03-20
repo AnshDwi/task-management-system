a5dc0def5ce4aaa6f37e2de4c073320a5de4b74b
AnshDwi <anshdwi21@gmail.com>
2026-03-20 10:01:47 +0530

    fixed schema + added priority

--- server/models/Task.js
+++ server/models/Task.js
@@ -12,6 +12,11 @@ const taskSchema = new mongoose.Schema(
       trim: true,
       default: "",
     },
+    priority: {
+  type: String,
+  enum: ["Low", "Medium", "High"],
+  default: "Medium",
+},
     status: {
       type: String,
       enum: ["pending", "completed"],
